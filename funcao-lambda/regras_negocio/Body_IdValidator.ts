c074567ad95997249762c190e1589221de9169cf
julian-gamboa-ensino <julian.gamboa.ms.2021@gmail.com>
2023-11-22 06:12:00 -0300

    ortografia https://github.com/julian-gamboa-bahia/aulas-serveless-2023/blob/aprimoramentos-nov-22/funcao-lambda/regras_negocio/README_regras.md

--- funcao-lambda/regras_negocio/Body_IdValidator.ts
+++ funcao-lambda/regras_negocio/Body_IdValidator.ts
@@ -1,16 +1,29 @@
 
+/**
+ * Represents a validator for validating an ID.
+ */
 export class Body_IdValidator {
 
     private id: any;
     private statusCode: number;
     private menssagensErro: string;
 
+    /**
+     * Creates an instance of Body_IdValidator.
+     * @param id - The ID to be validated.
+     * @param statusCode - The status code to be returned in case of validation failure.
+     * @param menssagensErro - The error messages to be logged in case of validation failure.
+     */
     constructor(id: any, statusCode: number, menssagensErro: string) {
         this.id = id
         this.statusCode = statusCode;
         this.menssagensErro = menssagensErro;
     }
 
+    /**
+     * Validates the ID.
+     * @returns The response status code if the ID is invalid, otherwise null.
+     */
     validateId() {
         const idString = this.id
 
@@ -26,8 +39,6 @@ export class Body_IdValidator {
 
         return null;
     }
-
-
 }
 
 
